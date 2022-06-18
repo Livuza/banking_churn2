515616ef945b0c44adfd763fcca8006285d51563
Charles Livuza <36395099+Livuza@users.noreply.github.com>
2022-06-19 00:16:24 +0300

    Update banking_churn.py

--- banking_churn.py
+++ banking_churn.py
@@ -8,4 +8,6 @@ url = "https://raw.githubusercontent.com/Livuza/ADS-April-2021/main/Assignments/
 banking_churn = pd.read_csv(url)
 banking_churn.head()
 banking_churn.describe()
+st.title("Bank Churn")
+
 
